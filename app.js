1b4c9fcc7a1812869eb92d553c227d828af969ed
João Miguel <joao.lsousa@sptech.school>
2026-06-11 12:34:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ var avisosRouter = require("./src/routes/avisos");
 var medidasRouter = require("./src/routes/medidas");
 var aquariosRouter = require("./src/routes/aquarios");
 var empresasRouter = require("./src/routes/empresas");
-//var resultadoRouter = require("./src/routes/resultados");
-//var tentativaRouter = require('./src/routes/tentativa');
+var resultadoRouter = require("./src/routes/resultados");
+var tentativaRouter = require('./src/routes/tentativa');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

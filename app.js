693e684332d27e42f6a6e55d24150c6c706bf67a
João Miguel <joao.lsousa@sptech.school>
2026-06-11 12:28:06 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+// var ambiente_processo = 'producao';
+var ambiente_processo = 'desenvolvimento';
+
+var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
+
+require("dotenv").config({ path: caminho_env });
+
+var express = require("express");
+var cors = require("cors");
+var path = require("path");
+var PORTA_APP = process.env.APP_PORT;
+var HOST_APP = process.env.APP_HOST;
+
+var app = express();
+
+var indexRouter = require("./src/routes/index");
+var usuarioRouter = require("./src/routes/usuarios");
+var avisosRouter = require("./src/routes/avisos");
+var medidasRouter = require("./src/routes/medidas");
+var aquariosRouter = require("./src/routes/aquarios");
+var empresasRouter = require("./src/routes/empresas");
+var resultadoRouter = require("./src/routes/resultados");
+var tentativaRouter = require('./src/routes/tentativa');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
+
+app.use("/", indexRouter);
+app.use("/usuarios", usuarioRouter);
+app.use("/avisos", avisosRouter);
+app.use("/medidas", medidasRouter);
+app.use("/aquarios", aquariosRouter);
+app.use("/empresas", empresasRouter);
+app.use("/resultados", resultadoRouter);
+app.use("/tentativas", tentativaRouter);
+
+app.listen(PORTA_APP, function () {
+    console.log(`
+    ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
+    ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
+    ##   ##  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##        ##   
+    ## # ##  ####     #####    ######   ##  ##   ######     ##     ######   ######   ##  ##     ##       ##    
+    #######  ##       ##  ##            ##  ##   ##  ##     ##     ##  ##            ##  ##     ##      ##     
+    ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
+    ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
+    
+    Servidor do seu site já está rodando!
+    Acesse: http://${HOST_APP}:${PORTA_APP}
+
+    Ambiente: ${process.env.AMBIENTE_PROCESSO}
+    `);
+});
